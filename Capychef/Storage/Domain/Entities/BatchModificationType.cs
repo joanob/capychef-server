8113330e2c3d2b63d51003630c4c7b0780608fc8
Joan Oltra Blasco <joltrablasco@gmail.com>
2026-04-04 23:44:56 +0200

    fix(storage): create batch modification type

---
+++ Capychef/Storage/Domain/Entities/BatchModificationType.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Capychef.Households.Domain.Entities;
+
+public class BatchModificationType
+{
+    private const string InitialConst = "C";
+
+    private readonly string _value;
+
+    private BatchModificationType(string value)
+    {
+        _value = value;
+    }
+
+    public static BatchModificationType Initial => new(InitialConst);
+
+    public static BatchModificationType From(string value)
+    {
+        return value switch
+        {
+            InitialConst => Initial,
+            _ => new BatchModificationType("")
+        };
+    }
+
+    public override string ToString()
+    {
+        return _value;
+    }
+}
+
+public class BatchModificationTypeConverter() : ValueConverter<BatchModificationType, string>(v => v.ToString(),
+    v => BatchModificationType.From(v));
